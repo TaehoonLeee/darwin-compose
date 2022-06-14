d84c7f58fe7b71c356139d8cd82e30b187502885
TaehoonLeee <lyc9290@gmail.com>
2022-06-14 18:10:49 +0900

    Android Compose

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -17,11 +17,17 @@ android {
             isMinifyEnabled = false
         }
     }
+    buildFeatures {
+        compose = true
+    }
+    composeOptions {
+        kotlinCompilerExtensionVersion = "1.2.0-beta03"
+    }
 }
 
 dependencies {
     implementation(project(":shared"))
-    implementation("com.google.android.material:material:1.4.0")
-    implementation("androidx.appcompat:appcompat:1.3.1")
-    implementation("androidx.constraintlayout:constraintlayout:2.1.0")
+    implementation(project(":ui-compose"))
+    implementation("androidx.activity:activity-compose:1.4.0")
+    implementation("androidx.appcompat:appcompat:1.4.2")
 }
--- androidApp/src/main/java/com/example/darwincompose/android/MainActivity.kt
+++ androidApp/src/main/java/com/example/darwincompose/android/MainActivity.kt
@@ -2,19 +2,15 @@ package com.example.darwincompose.android
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.example.darwincompose.Greeting
-import android.widget.TextView
-
-fun greet(): String {
-    return Greeting().greeting()
-}
+import androidx.activity.compose.setContent
+import com.example.darwincompose.uicompose.MainContent
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
 
-        val tv: TextView = findViewById(R.id.text_view)
-        tv.text = greet()
+        setContent {
+            MainContent()
+        }
     }
 }
